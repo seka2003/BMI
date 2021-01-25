c715b0d1d668d1e6ff6e3f99dba9f7ca10bdb5ee
KGs <43301739+seka2003@users.noreply.github.com>
2021-01-26 00:18:55 +0700

    อธิบายรายละเอือดเพิ่มเติม
    อธิบายวิธีที่แตกต่างที่สามารถใช้งานได้เช่นกัน และส่วนที่ทำไมอันนี้ถึงใช้อย่างงี้(กันตัวเองลืม)

--- BMI.py
+++ BMI.py
@@ -3,9 +3,17 @@
 kg=int(input('Enter Your mass : '))
 cm=int(input('Enter your Tall : '))
 
-
+'''
+ตรงเอา cm ไปหาร100 ก่อนค่อยกลับเข้ามาคำนวณของ resl ก็ได้จะได้ดูง่ายกว่าของผม
+เช่น 
+m = cm/100
+resl = kg/m**2
+ก็ได้เช่นกัน
+'''
 resl=kg/(cm/100)**2
-print('You BMI is : '+ str(resl))
+
+
+print('You BMI is : '+ str(resl)) #ต้องใช้ str เพื่อให้ resl เป็น String สำหรับแสดงผล
 
 if resl <= 18.5:
     print('Your are lean')
